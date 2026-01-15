93ec824dc44b5e5c024519da5d36f0226e2328ac
Soham Sachin Phatak <sohamphatakssp@gmail.com>
2026-01-15 18:37:26 +0530

    signup with google

--- src/components/customer/Login.jsx
+++ src/components/customer/Login.jsx
@@ -137,7 +137,7 @@ export default function Login() {
                 <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
               </svg>
             </div>
-            <span className="btn-text">Continue with Google</span>
+            <span className="btn-text">Login with Google</span>
             <div className="btn-shimmer"></div>
           </button>
         </form>
